284bf60306e37104f6e963ad4fc1ec14661ab110
Čajka Vladimír <cajka.vlada@gmail.com>
2024-09-26 16:59:46 +0200

    Sanitize old component removal

--- src/features/quiz/components/questionAnswer.tsx
+++ src/features/quiz/components/questionAnswer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { SanitizedHTML } from "~/components/common/SanitizedHTML";
 import { Button } from "~/components/ui";
 import type { QuestionProps } from "~/features/questions/types";
 import { nextQuestion, previousQuestion, restartQuizSession } from "../actions";

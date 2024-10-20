1337e0a0b5165dfcfcb743a5c8f823a9f6c8fa36
Čajka Vladimír <cajka.vlada@gmail.com>
2024-10-20 09:36:38 +0200

    rate limit creator refactoring

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -1,28 +1,14 @@
-import { Ratelimit } from "@upstash/ratelimit";
+import { Ratelimit, type Duration } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-// Create a new ratelimiter, that allows 10 requests per 10 seconds
-export const topicRatelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(2, "10 s"),
-  analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: "@upstash/ratelimit",
-});
+function createRateLimit(count: number, duration: Duration) {
+  return new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(count, duration),
+    analytics: true,
+    prefix: "@upstash/ratelimit",
+  });
+}
 
-// Create a new ratelimiter, that allows 10 requests per 10 seconds
-export const questionRatelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(3, "10 s"),
-  analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: "@upstash/ratelimit",
-});
+export const topicRatelimit = createRateLimit(2, "10 s");
+export const questionRatelimit = createRateLimit(3, "10 s");
